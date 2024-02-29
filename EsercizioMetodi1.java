86ea289ded3d34469ef0702f944b6ab94e9d9e29
Sebastiano <160122164+Sebahhhhh@users.noreply.github.com>
2024-02-29 11:32:50 +0100

    Add files via upload

---
+++ EsercizioMetodi1.java
@@ -0,0 +1,20 @@
+public class EsercizioMetodi1 {
+    public static void main(String[] args) {
+        
+        metodo1();
+        metodo2();
+        metodo3();
+    }
+
+    private static void metodo1() {
+        System.out.println("Ecco il primo.");
+    }
+
+    private static void metodo2() {
+        System.out.println("Ecco il secondo.");
+    }
+
+    private static void metodo3() {
+        System.out.println("E per finire ecco il terzo.");
+    }
+}
