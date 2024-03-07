ec269b1aa0e466945993f3f0281d805cc40f02b2
Sebastiano <160122164+Sebahhhhh@users.noreply.github.com>
2024-03-07 11:53:35 +0100

    Add files via upload

---
+++ InvertiStringa.java
@@ -0,0 +1,25 @@
+
+import java.util.Scanner;
+
+public class InvertiStringa {
+public static void main(String[] args) { 
+
+  Scanner in = new Scanner(System.in);
+   System.out.print("Inserisci una parola: ");
+  String parola = in.nextLine();
+
+
+   private static void inversione(String parola){
+    String invertita = "";
+
+    for(int i = str.length() - 1; i>=0; i--){
+        invertita += str.charAt(i);
+    }
+    System.out.println(invertita);
+}
+
+  public static void main(String[] args){
+      inversione(parola);
+  }
+}
+}
