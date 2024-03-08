b1ab0c4e0eb15266de57c4d211f5a4026eb911c3
Sebastiano <160122164+Sebahhhhh@users.noreply.github.com>
2024-03-08 13:42:58 +0100

    Add files via upload

---
+++ ControllaFraseMetodiCompleto.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+public class ControllaFraseMetodiCompleto {
+  public static void main(String[] args) {
+    Scanner in = new Scanner(System.in);
+    System.out.print("Inserisci una frase: ");
+    String frase = in.nextLine();
+    controllaCaratteri(frase);
+  }
+  public static void controllaCaratteri(String frase) {
+    String vocali = "";
+    String consonanti = "";
+    int spazi = 0;
+    String caratteri = "";
+    for (int i = 0; i < frase.length(); i++) {
+      if (isVocal(frase.charAt(i))) {
+        vocali = vocali + frase.charAt(i);
+      } else if (isConsonant(frase.charAt(i))) {
+        consonanti = consonanti + frase.charAt(i);
+      } else if (frase.charAt(i) == ' ') {
+        spazi++;
+      } else {
+        caratteri = caratteri + frase.charAt(i);
+      }
+    }
+    System.out.println("Le vocali sono: " + vocali);
+    System.out.println("Le consonanti sono: " + consonanti);
+    System.out.println("I caratteri speciali sono: " + caratteri);
+    System.out.println("Gli spazi sono: " + spazi);
+  }
+  public static boolean isVocal(char c) {
+    return c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u' || c == 'A' || c == 'E' || c == 'I' || c == 'O' || c == 'U';
+  }
+  public static boolean isConsonant(char c) {
+      return c == 'b' || c == 'c' || c == 'd' || c == 'f' || c == 'g' || c == 'h' || c == 'j' || c == 'k' || c == 'l' || c == 'm' || c == 'n' || c == 'p' || c == 'q' || c == 'r' || c == 's' || c == 't' || c == 'v' || c == 'w' || c == 'x' || c == 'y' || c == 'z' || c == 'B' || c == 'C' || c == 'D' || c == 'F' || c == 'G' || c == 'H' || c == 'J' || c == 'K' || c == 'L' || c == 'M' || c == 'N' || c == 'P' || c == 'Q' || c == 'R' || c == 'S' || c == 'T' || c == 'V' || c == 'W' || c == 'X' || c == 'Y' || c == 'Z';
+    }
+  }
